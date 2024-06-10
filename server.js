6e7eff8bdf1e7d86b52e241a078c4063a9b1b370
jimmyNicholas <jimmynicholas@duck.com>
2024-06-10 10:36:49 +1000

    Imported the html and css files from the previous project. Added a index.js file. Solved package.json bug and updated entry point

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ import config from './config/config.js';
 
 const PORT = config.port || 3000;
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
-});
+
 
 app.listen(PORT, () => {
     console.log('Server is running on port: ' + PORT)
